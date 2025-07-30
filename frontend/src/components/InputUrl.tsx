fd1b5111e64391a7073b91d33d5e69f294f767cd
Jenny <jennywei0102@gmail.com>
2025-07-30 17:32:33 +0200

    Change page from input to result after clicking Shorten. Show the shortened URL on the screen

--- frontend/src/components/InputUrl.tsx
+++ frontend/src/components/InputUrl.tsx
@@ -8,13 +8,11 @@ const InputUrl = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      // const response =
-      await axios.post("/api/urls/shorten", { originUrl });
-      // const data = response.json();
-      // const shortenedUrl = data.shortenedUrl;
-      alert("See what you got~");
+      const res = await axios.post("/api/urls/shorten", { originUrl });
+      const shortenedUrl = res.data.shortenedUrl;
+      console.log("See what you got~");
       setOriginUrl("");
-      navigate("/result");
+      navigate(`/result?short=${shortenedUrl}`);
     } catch (err) {
       alert("Fail to submit URL");
       console.log(err);
--- frontend/src/components/ResultUrl.tsx
+++ frontend/src/components/ResultUrl.tsx
@@ -1,5 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
 const ResultUrl = () => {
-  return <h1>This is result page</h1>;
+  const [searchParams] = useSearchParams();
+  const shortenedUrl = searchParams.get("short");
+
+  if (!shortenedUrl) return <p>Short URL not found</p>;
+
+  // useEffect(() => {
+  //   const fecthUrl = async () => {
+  //     const res = await axios.get("", {
+  //       params: shortenedUrl ? { shortenedUrl } : {},
+  //     });
+  //   };
+  // });
+
+  return (
+    <div>
+      <h3>This is your shortened URL:</h3>
+      <p>{`https://localhost:3000/${shortenedUrl}`}</p>
+    </div>
+  );
 };
 
 export default ResultUrl;
