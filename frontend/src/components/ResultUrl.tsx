e0f2c8bc6367ac27ad4e2eac1cc15403bae8c2d7
Jenny <jennywei0102@gmail.com>
2025-07-31 14:55:04 +0200

    Able to show origin and shortened URLs, copy shortened URL but it's wrong

--- frontend/src/components/ResultUrl.tsx
+++ frontend/src/components/ResultUrl.tsx
@@ -1,25 +1,48 @@
 import axios from "axios";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
-const ResultUrl = () => {
+const ResultUrl: React.FC = () => {
   const [searchParams] = useSearchParams();
   const shortenedUrl = searchParams.get("short");
+  const [originUrl, setOriginUrl] = useState<string>("");
 
-  if (!shortenedUrl) return <p>Short URL not found</p>;
+  useEffect(() => {
+    if (!shortenedUrl) return;
+    axios
+      .get(`/api/urls/${shortenedUrl}`)
+      .then((res) => {
+        setOriginUrl(res.data.originUrl);
+      })
+      .catch((err) => console.log("Failed to fetch origin URL", err));
+  }, [shortenedUrl, originUrl]);
 
-  // useEffect(() => {
-  //   const fecthUrl = async () => {
-  //     const res = await axios.get("", {
-  //       params: shortenedUrl ? { shortenedUrl } : {},
-  //     });
-  //   };
-  // });
+  // copy後出現的是http://localhost:5173/14e710da，無法貼上後到對的網站
+  const handleCopy = () => {
+    const fullShortUrl = `${window.location.origin}/${shortenedUrl}`;
+    navigator.clipboard.writeText(fullShortUrl).then(() => {
+      alert("Short URL copied to clipboard!");
+    });
+  };
+
+  if (!shortenedUrl) return <p>Cannot find shorted URL parameter</p>;
+  if (!originUrl) return <p>Loading...</p>;
+
+  const fullShourtenUrl = `${window.location.origin}/${shortenedUrl}`;
 
   return (
     <div>
-      <h3>This is your shortened URL:</h3>
-      <p>{`https://localhost:3000/${shortenedUrl}`}</p>
+      <p>This is your shortened URL:</p>
+      {/* Click the link to redirect to the page */}
+      <a href={originUrl} target="_blank" rel="noopener noreferrer">
+        {fullShourtenUrl}
+      </a>
+      {/* Button to copy the link. */}
+      <button onClick={handleCopy}>Copy</button>
+      <h3>Origin URL you input:</h3>
+      <a href={originUrl} target="_blank" rel="noopener noreferrer">
+        {originUrl}
+      </a>
     </div>
   );
 };
